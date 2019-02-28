a3d420e38719508306cb1c766513c72b9402240e
mudkipmaster <acg59@drexel.edu>
2019-02-28 14:22:18 -0500

    moved parse

--- main.py
+++ main.py
@@ -1,56 +1,5 @@
-import sys
-from os import listdir
-
-from image import Image
-from constants import FilePath, InputFile
-
-
-def get_data_sets():
-    data_dir = FilePath.pwd + FilePath.data
-    all_data_sets = listdir(data_dir)
-
-    data_set_map = {}
-    for data_set in all_data_sets:
-        key = data_set.split('_')[0]
-        absolute_path = data_dir + data_set
-        data_set_map.update({key: absolute_path})
-
-    return data_set_map
-
-
-def parse_images(file_path):
-    # Returns a list of Image objects
-    images = []
-    file = open(data_set, 'r')
-
-    for line_no, line in enumerate(file):
-        if line_no < InputFile.lines_to_skip:
-            continue
-
-        data = line.split(' ')
-
-        id = line_no - InputFile.lines_to_skip
-        orientation = data[0]
-        number_of_tags = data[1]
-        tags = data[2:]
-
-        image = Image(id, orientation, tags)
-        images.append(image)
-
-    file.close()
-
-    return images
-
+from Parser import Parser
 
 if __name__ == "__main__":
-    # Command line args, list files to test
-    # If no file names are supplied then test all
-    all_data_sets = get_data_sets()
-
-    keys_to_parse = sys.argv[1:] or all_data_sets.keys()
-
-    data_sets = [file for key, file in all_data_sets.iteritems() if key in keys_to_parse]
-
-    for data_set in data_sets:
-        print('Parsing', data_set)
-        images = parse_images(data_set)
+    parser = Parser.parse("a")
+    print(len(parser))
