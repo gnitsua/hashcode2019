22f1b5e859eb7c0941b730d68c5b0fae7813091d
mudkipmaster <acg59@drexel.edu>
2019-02-28 14:58:53 -0500

    added suptraction to distance

--- image.py
+++ image.py
@@ -5,3 +5,7 @@ class Image(object):
         self.id = int(id)
         self.orientation = Orientation.horizontal if orientation == 'H' else Orientation.vertical
         self.tags = set(tags)
+
+
+    def __sub__(self, other):
+        return self.tags-other.tags
