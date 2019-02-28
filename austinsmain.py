190f7b29f0af0af07eb4138032ac745857d86a14
mudkipmaster <acg59@drexel.edu>
2019-02-28 15:02:25 -0500

    added austinsmain

---
+++ austinsmain.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from Parser import Parser
+from slideshow import SlideShow
+
+# import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    dataset_letter = "b"
+    dataset = Parser.parse(dataset_letter)
+
+    with open(dataset_letter+"_distances.txt","r") as file:
+
+        distance = {}
+        for i, image1 in enumerate(dataset):
+            for j, image2 in enumerate(dataset):
+                temp_slideshow = SlideShow([image1, image2])
+                file.write("("+str(i), str(j)+"):"+str(temp_slideshow.score()))
+                # distance["("+str(i), str(j)+")"] = temp_slideshow.score()
+                # datasets[i][j] = (image1.tags,image2.tags)
+
+
+
+
+
+    # np.save("distance" + dataset_letter + ".npy", distance)
+    print(distance)
+    # plt.matshow(distance)
+    # plt.show()
+
+    # print(dataset)
+    # solver = Solver(dataset)
+    # print(solver.solve())
