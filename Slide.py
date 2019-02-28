cf9939396004b35279c0c9c750216877975adf68
mudkipmaster <acg59@drexel.edu>
2019-02-28 13:37:02 -0500

    added score output and slideshow

--- Slide.py
+++ Slide.py
@@ -1,8 +1,9 @@
 from constants import Orientation
 
+
 class Slide():
     def __init__(self, image1, image2):
-        assert(image1 != None)
+        assert (image1 != None)
         self.image1 = image1
         self.image2 = image2
         if (self.image2 != None):
@@ -14,15 +15,15 @@ class Slide():
     @property
     def tags(self):
         result = self.image1.tags
-        if(self.image2 != None):
+        if (self.image2 != None):
             result = result | self.image2.tags
-        return  result
+        return result
 
     def __sub__(self, other):
         common_tags = self.tags.intersection(other.tags)
         tags_only_in_1 = self.tags.difference(other.tags)
         tags_only_in_2 = other.tags.difference(self.tags)
-        return min(common_tags,tags_only_in_1,tags_only_in_2)
+        return min(len(common_tags), len(tags_only_in_1), len(tags_only_in_2))
 
     def __str__(self):
-        return "Slide ("+str(self.image1)+","+str(self.image2)+")"
+        return "Slide (" + str(self.image1) + "," + str(self.image2) + ")"
---
+++ slideshow.py
@@ -0,0 +1,20 @@
+class SlideShow():
+    def __init__(self, slides):
+        assert (len(slides) > 1)
+        self.slides = slides
+
+    def score(self):
+        score = 0
+        for i in range(1, len(self.slides)):
+            score += self.slides[i] - self.slides[i - 1]
+
+        return score
+
+    def __str__(self):
+        result = str(len(self.slides)) + "\n"
+        for slide in self.slides:
+            result += str(slide.image1.id) + " "
+            if (slide.image2 != None):
+                result += str(slide.image2.id)
+            result += "\n"
+        return result
